725b21008df8e8079af010b6b1375d839aba5496
S.Guna Balan <162681185+Guna-Balan-S@users.noreply.github.com>
2025-01-05 14:58:03 +0530

    Update TriangleStarpattern.java

--- TriangleStarpattern.java
+++ TriangleStarpattern.java
@@ -3,9 +3,8 @@ public class TriangleStarpattern{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int row = sc.nextInt();
-        int column = sc.nextInt();
         for (int i=1; i<=row; i++){
-            for(int j= 1; j<=column; j++){
+            for(int j= 1; j<=i; j++){
                 System.out.print("*");
             }
             System.out.println();
